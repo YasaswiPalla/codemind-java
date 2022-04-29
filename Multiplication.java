e9268f871a092e83e4456b84df87aac872917521
YasaswiPalla <96292517+YasaswiPalla@users.noreply.github.com>
2022-04-29 08:00:57 +0530

    import java.util.Scanner; class modulus {     public static void main(String args[])     {         int c,a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         c=a*b;         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class modulus
+{
+    public static void main(String args[])
+    {
+        int c,a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a*b;
+        System.out.println(c);
+    }
+}
