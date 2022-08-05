302e494e1383e6f59b52f7820aec4d77c06eb3d2
YasaswiPalla <96292517+YasaswiPalla@users.noreply.github.com>
2022-08-06 00:02:00 +0530

    import java.util.Scanner; class even {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in); 	int n,a[],i; 	int e=0; 	n=sc.nextInt(); 	a=new int[n]; 	for(i=0;i<n;i++) 	{ 	a[i]=sc.nextInt();     } 	for(i=0;i<n;i++) 	{     if(a[i]%2==0)     {     	e=a[i]; 	}     }     System.out.printf("%d",e);     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class even
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+	int n,a[],i;
+	int e=0;
+	n=sc.nextInt();
+	a=new int[n];
+	for(i=0;i<n;i++)
+	{
+	a[i]=sc.nextInt();
+    }
+	for(i=0;i<n;i++)
+	{
+    if(a[i]%2==0)
+    {
+    	e=a[i];
+	} 
+    }
+    System.out.printf("%d",e);
+    }
+}
