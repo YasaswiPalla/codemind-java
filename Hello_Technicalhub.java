1f24f9557756fb9290d99c2fc9916621e0f42016
YasaswiPalla <96292517+YasaswiPalla@users.noreply.github.com>
2022-08-25 12:07:47 +0530

    import java.util.Scanner; class Hello {     public static void main(String args[])     {         String a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(a);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Hello
+{
+    public static void main(String args[])
+    {
+        String a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(a);
+    }
+}
